db3a70780206ed804cc095f5020ff7aaf22520cf
Nikita Karamov <nick@karamoff.ru>
2019-04-03 13:07:32 +0300

    Add index controller

---
+++ src/main/java/ru/karamoff/mcdrive/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package ru.karamoff.mcdrive.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/signin")
+    public String loginPage() {
+        return "signin";
+    }
+}
