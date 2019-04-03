852fd13ab3e74857323d5eb381ec96281dd965e0
Nikita Karamov <nick@karamoff.ru>
2019-04-03 13:06:46 +0300

    Initialize project

---
+++ src/main/java/ru/karamoff/mcdrive/McdriveApplication.java
@@ -0,0 +1,13 @@
+package ru.karamoff.mcdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McdriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McdriveApplication.class, args);
+    }
+
+}
