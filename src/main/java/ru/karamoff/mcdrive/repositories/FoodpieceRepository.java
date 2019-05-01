6f545ed1045ff4f7f8223e2d87eb1c91f5fa4776
Nikita Karamov <nick@karamoff.ru>
2019-05-01 22:41:08 +0300

    Add ordering by id for repositories

--- src/main/java/ru/karamoff/mcdrive/repositories/FoodpieceRepository.java
+++ src/main/java/ru/karamoff/mcdrive/repositories/FoodpieceRepository.java
@@ -3,5 +3,8 @@ package ru.karamoff.mcdrive.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.karamoff.mcdrive.models.Foodpiece;
 
+import java.util.List;
+
 public interface FoodpieceRepository extends JpaRepository<Foodpiece, Long> {
+    List<Foodpiece> findAllByOrderByIdAsc();
 }
---
+++ src/main/java/ru/karamoff/mcdrive/repositories/IngredientRepository.java
@@ -0,0 +1,10 @@
+package ru.karamoff.mcdrive.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.karamoff.mcdrive.models.Ingredient;
+
+import java.util.List;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    List<Ingredient> findAllByOrderByIdAsc();
+}
